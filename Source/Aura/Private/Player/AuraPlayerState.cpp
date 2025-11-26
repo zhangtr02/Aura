b2fca43030a283b752c6f767eea916982cce8503
Tianrong Zhang <zhangtr02@gmail.com>
2025-11-26 20:58:32 +0800

    Constructing the ASC and AS

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -3,7 +3,20 @@
 
 #include "Player/AuraPlayerState.h"
 
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
 AAuraPlayerState::AAuraPlayerState()
 {
 	SetNetUpdateFrequency(100.0f);
+	
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	
+	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+}
+
+UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
 }
